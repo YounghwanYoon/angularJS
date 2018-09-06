81815f018b387563a14b4b88536fed2725b554e1
YounghwanYoon <ray.yoon87@gmail.com>
2018-09-06 18:21:31 -0400

    Completed getter and setter for AngularJS

--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,17 +1,4 @@
-class Point{
-    x:number;
-    y:number;
-
-    draw(){
-        console.log('X: ' + this.x + ', Y: '+ this.y);
-    }
-
-    getDistance(another:Point){
-
-    }
-}
-
-let points = new Point();
-points.x = 1;
-points.y = 2;
+let points = new Point(1,2);
+let x = points.x;
+points.x = 10;
 points.draw();
---
+++ ts-hello/point.ts
@@ -0,0 +1,33 @@
+class Point{
+    //? means optional. This feature allows 
+    //us to have a constructor without any parameter. 
+    constructor(private _x?:number, private _y?:number){
+
+    }
+    draw(){
+        console.log('X: ' + this._x + ', Y: '+ this._y);
+    }
+
+    getDistance(another:Point){
+
+    }
+
+    get x(){
+        return this._x;
+    }
+    get y(){
+        return this._y;
+    }
+
+    set x(value){
+        if(value <0)
+            throw new Error('value cannot be less than 0.');
+        this._x = value;
+    }
+    set y(value){
+        if(value <0)
+            throw new Error('value cannot be less than 0.');
+        this._x = value;
+    }
+
+}
