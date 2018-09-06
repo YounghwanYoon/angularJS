5e63f8a4c4e1a3c8143f18e5c8b6d5a0d0d50c1c
YounghwanYoon <ray.yoon87@gmail.com>
2018-09-06 15:15:27 -0400

    Pushing test

---
+++ ts-hello/main.js
@@ -0,0 +1,12 @@
+var Point = /** @class */ (function () {
+    function Point() {
+    }
+    Point.prototype.draw = function () {
+        console.log('X: ' + this.x + ', Y: ' + this.y);
+    };
+    Point.prototype.getDistance = function (another) {
+    };
+    return Point;
+}());
+var points;
+points.draw;
